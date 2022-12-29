c0293690275cd2e3d52868df325e9f0cb5e1dd40
Inzamum-Ul-Haque <inzamum.inu@gmail.com>
2022-12-29 18:42:39 +0600

    chat form design done

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,7 +1,25 @@
 import React from "react";
+import cam from "../../img/cam.png";
+import add from "../../img/add.png";
+import more from "../../img/more.png";
+import Messages from "../Messages/Messages";
+import Input from "../Input/Input";
 
 const Chat = () => {
-  return <div className="chat">Chat</div>;
+  return (
+    <div className="chat">
+      <div className="chatInfo">
+        <span>Janet</span>
+        <div className="chatIcons">
+          <img src={cam} alt="" />
+          <img src={add} alt="" />
+          <img src={more} alt="" />
+        </div>
+      </div>
+      <Messages />
+      <Input />
+    </div>
+  );
 };
 
 export default Chat;
--- src/components/Chats/Chats.js
+++ src/components/Chats/Chats.js
@@ -13,6 +13,36 @@ const Chats = () => {
           <p>Hello</p>
         </div>
       </div>
+      <div className="userChat">
+        <img
+          src="https://images.pexels.com/photos/14845056/pexels-photo-14845056.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+          alt=""
+        />
+        <div className="userChatInfo">
+          <span>Janet</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="userChat">
+        <img
+          src="https://images.pexels.com/photos/14845056/pexels-photo-14845056.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+          alt=""
+        />
+        <div className="userChatInfo">
+          <span>Janet</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="userChat">
+        <img
+          src="https://images.pexels.com/photos/14845056/pexels-photo-14845056.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+          alt=""
+        />
+        <div className="userChatInfo">
+          <span>Janet</span>
+          <p>Hello</p>
+        </div>
+      </div>
     </div>
   );
 };
