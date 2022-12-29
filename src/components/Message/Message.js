d09fa99c0bbab09d6ebb86024286c1b4aea2ca3f
Inzamum-Ul-Haque <inzamum.inu@gmail.com>
2022-12-29 20:42:18 +0600

    authentication

--- src/components/Message/Message.js
+++ src/components/Message/Message.js
@@ -1,7 +1,24 @@
 import React from "react";
 
 const Message = () => {
-  return <div className="message"></div>;
+  return (
+    <div className="message owner">
+      <div className="messageInfo">
+        <img
+          src="https://images.pexels.com/photos/14845056/pexels-photo-14845056.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+          alt=""
+        />
+        <span>just now</span>
+      </div>
+      <div className="messageContent">
+        <p>hello</p>
+        <img
+          src="https://images.pexels.com/photos/14845056/pexels-photo-14845056.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+          alt=""
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Message;
---
+++ src/firebase/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBDtBLYHVPjHOGwdEDr3YGT-glGYDvjoBc",
+  authDomain: "firechat-24b94.firebaseapp.com",
+  projectId: "firechat-24b94",
+  storageBucket: "firechat-24b94.appspot.com",
+  messagingSenderId: "706886435178",
+  appId: "1:706886435178:web:eaa730a1887e22fe74a513",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
