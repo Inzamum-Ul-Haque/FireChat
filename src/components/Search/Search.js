b7408882c000f4bdf4cb36067eb3a657043c73d0
Inzamum-Ul-Haque <inzamum.inu@gmail.com>
2022-12-30 01:10:24 +0600

    search function

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,11 +1,22 @@
-import React, { useState } from "react";
-import { collection, getDocs, query, where } from "firebase/firestore";
+import React, { useContext, useState } from "react";
+import {
+  collection,
+  doc,
+  getDocs,
+  query,
+  serverTimestamp,
+  setDoc,
+  updateDoc,
+  where,
+} from "firebase/firestore";
 import { db } from "../../firebase/firebase";
+import { AuthContext } from "../../context/AuthContext";
 
 const Search = () => {
   const [username, setUsername] = useState("");
   const [user, setUser] = useState("");
   const [error, setError] = useState(false);
+  const { currentUser } = useContext(AuthContext);
 
   const handleSearch = async () => {
     const usersRef = collection(db, "users");
@@ -25,7 +36,43 @@ const Search = () => {
     e.code === "Enter" && handleSearch();
   };
 
-  const handleSelect = () => {};
+  const handleSelect = async () => {
+    // check if the group chat exists, if not create a new one
+    const combinedId =
+      currentUser.uid > user.uid
+        ? currentUser.uid + user.uid
+        : user.uid + currentUser.uid;
+    try {
+      const res = getDocs(db, "chats", combinedId);
+
+      if (!res.exists()) {
+        // create a new chat in chats collection
+        await setDoc(doc(db, "chats", combinedId), { messages: [] });
+        // create a new user chat
+
+        await updateDoc(doc(db, "userChats", currentUser.uid), {
+          [combinedId + "userInfo"]: {
+            uid: user.uid,
+            displayName: user.displayName,
+            photoURL: user.photoURL,
+          },
+          [combinedId + ".date"]: serverTimestamp(),
+        });
+
+        await updateDoc(doc(db, "userChats", user.uid), {
+          [combinedId + "userInfo"]: {
+            uid: currentUser.uid,
+            displayName: currentUser.displayName,
+            photoURL: currentUser.photoURL,
+          },
+          [combinedId + ".date"]: serverTimestamp(),
+        });
+      }
+    } catch (error) {
+      setError(true);
+    }
+    // create user chats
+  };
 
   return (
     <div className="search">
